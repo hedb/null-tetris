0869191ceb543a4fe2f9bd839dbf988503b16312
hedb <hedbn@wix.com>
2018-11-09 21:24:23 +0200

    Stable. Need to add diaogonal. Need to explode to center

--- js/render.js
+++ js/render.js
@@ -4,10 +4,12 @@ ctx.font = "30px Arial";
 var W = 300, H = 600;
 var BLOCK_W = W / COLS, BLOCK_H = H / ROWS;
 
+exploding_letters = null
+explosionPixelSize = 0
+
 // draw a single square at (x, y)
 function drawLetter( x, y , letter) {
-    ctx.fillText(letter,x*BLOCK_W,y*BLOCK_H);
-
+    ctx.fillText(letter,x,y);
 }
 
 // draws the board and the moving shape
@@ -19,7 +21,7 @@ function render() {
         for ( var y = 0; y < ROWS; ++y ) {
             if ( board[ y ][ x ] ) {
                 ctx.fillStyle = colors[ board[ y ][ x ] - 1 ];
-                drawLetter( x, y ,board[ y ][ x ]);
+                drawLetter( x*BLOCK_W, y*BLOCK_H ,board[ y ][ x ]);
             }
         }
     }
@@ -27,5 +29,38 @@ function render() {
     ctx.fillStyle = 'red';
     ctx.strokeStyle = 'black';
     ctx.fillStyle = colors[ current - 1 ];
-    drawLetter( currentX, currentY , current);   
+    drawLetter( currentX*BLOCK_W, currentY*BLOCK_H , current);
+    
+    if (exploding_letters) {
+        render_exploding_letters()
+    }
+}
+
+function renderStartExplosion(exploding_letters1) {
+    exploding_letters = exploding_letters1
+}
+
+function renderEndExplosion() {
+    exploding_letters = null
+    explosionPixelSize = 0
+}
+
+function render_exploding_letters() {
+    defaultFont = ctx.font
+    ctx.font =  (30 + explosionPixelSize ) +"px Arial";
+    explosionPixelSize += 2
+
+    // middleWord = [ 
+    //     (exploding_letters[0][1] + exploding_letters[3][1] )  / 2 ,  
+    //     (exploding_letters[0][0] + exploding_letters[3][0] )  / 2 ,  
+    // ]
+
+    tmp_BLOCK_W = BLOCK_W //+ explosionPixelSize; 
+    tmp_BLOCK_H = BLOCK_H //+ explosionPixelSize; 
+
+    for (var i=0; i<exploding_letters.length ;i++) {
+        drawLetter(exploding_letters[i][1] *tmp_BLOCK_W , exploding_letters[i][0] *tmp_BLOCK_H ,  
+            board[ exploding_letters[i][0] ][ exploding_letters[i][1] ]  )
+    }
+    ctx.font = defaultFont 
 }
--- js/tetris.js
+++ js/tetris.js
@@ -2,6 +2,8 @@ var COLS = 10, ROWS = 20;
 var board = [];
 var lose;
 var interval;
+var tickDuration = 400;
+var tickCounter = 1;
 var intervalRender;
 var current; // current moving letter
 var currentX, currentY; // position of current shape
@@ -23,7 +25,7 @@ function newShape() {
     // new shape starts to move
     freezed = false;
     // position where the shape will evolve
-    currentX = 5;
+    currentX = Math.floor( Math.random() * COLS );;
     currentY = 0;
 }
 
@@ -37,8 +39,24 @@ function init() {
     }
 }
 
+var isExploding = false
+
+function explodeBoardStart(currentX,currentY,nullCoordinates) {
+    isExploding = true
+    clearInterval( interval );
+    renderStartExplosion(nullCoordinates)
+    setTimeout(explodeBoardEnd,2000)
+}
+
+function explodeBoardEnd(currentX,currentY) {
+    isExploding = false
+    renderEndExplosion()
+    newGame()
+}
+
 // keep the element moving down, creating new shapes and clearing lines
-function tick() {
+function tick(triggerNewTick) {
+    
     if ( valid( 0, 1 ) ) {
         ++currentY;
     }
@@ -46,13 +64,28 @@ function tick() {
     else {
         freeze();
         valid(0, 1);
-        checkForNulls(currentX,currentY)
+        nullCoordinates = []
+        if (checkForNulls(currentX,currentY,nullCoordinates)) {
+            explodeBoardStart(currentX,currentY,nullCoordinates)
+        }
         if (lose) {
             clearAllIntervals();
             return false;
         }
         newShape();
     }
+
+    if (typeof triggerNewTick == 'undefined' || triggerNewTick) {
+        
+        // debug
+        // tickDuration = 1000 
+        // prod
+        tickDuration = Math.cos(tickCounter/20)*100+ Math.max(0,200-tickCounter) + 100 
+        
+        interval = setTimeout(tick, tickDuration );
+        tickCounter ++;
+    }
+
 }
 
 // stop shape at its position and fix it to board
@@ -69,8 +102,9 @@ function up( current ) {
 
 
 
-function checkForNulls(changedX,changedY) {
+function checkForNulls(changedX,changedY,nullCoordinates) {
     var isNullOnTheBoard = false;
+    //LTR
     for (var offset = -1*WordLength + 1; offset <= 0 && !isNullOnTheBoard ;offset ++) {
         if (
             ( typeof board[changedY][changedX + offset] != 'undefined' ) && 
@@ -78,20 +112,73 @@ function checkForNulls(changedX,changedY) {
          ) {
             isNullOnTheBoard = true
             for (var i = 0;i<letters.length && isNullOnTheBoard ;i++) {
+                nullCoordinates.push([changedY,changedX + offset + i])
                 if (letters[i] != board[changedY][changedX + offset + i]) {
+                    nullCoordinates.length = 0
                     isNullOnTheBoard = false
                 }
             }
          }
     }
-    if (isNullOnTheBoard) {
-        check = true
+    //RTL
+    for (var offset = -1*WordLength + 1; offset <= 0 && !isNullOnTheBoard ;offset ++) {
+        if (
+            ( typeof board[changedY][changedX - offset] != 'undefined' ) && 
+            ( typeof board[changedY][changedX - (offset + WordLength - 1) ] != 'undefined' )
+         ) {
+            isNullOnTheBoard = true
+            for (var i = 0;i<letters.length && isNullOnTheBoard ;i++) {
+                nullCoordinates.push([changedY,changedX - (offset + i)])
+                if (letters[i] != board[changedY][changedX - (offset + i)]) {
+                    nullCoordinates.length = 0
+                    isNullOnTheBoard = false
+                }
+            }
+         }
+    }
+    //UTD
+    for (var offset = -1*WordLength + 1; offset <= 0 && !isNullOnTheBoard ;offset ++) {
+        if (
+            ( typeof board[changedY + offset] != 'undefined' ) && 
+            ( typeof board[changedY + offset][changedX] != 'undefined' ) && 
+            ( typeof board[changedY + offset + WordLength - 1 ] != 'undefined' ) &&
+            ( typeof board[changedY + offset + WordLength - 1 ][changedX] != 'undefined' )
+         ) {
+            isNullOnTheBoard = true
+            for (var i = 0;i<letters.length && isNullOnTheBoard ;i++) {
+                nullCoordinates.push([changedY + offset + i,changedX])
+                if (letters[i] != board[changedY + offset + i][changedX]) {
+                    nullCoordinates.length = 0
+                    isNullOnTheBoard = false
+                }
+            }
+         }
+    }
+    //DTU
+    for (var offset = -1*WordLength + 1; offset <= 0 && !isNullOnTheBoard ;offset ++) {
+        if (
+            ( typeof board[changedY - offset] != 'undefined' ) && 
+            ( typeof board[changedY - offset][changedX] != 'undefined' ) && 
+            ( typeof board[changedY - (offset + WordLength - 1) ] != 'undefined' ) &&
+            ( typeof board[changedY - (offset + WordLength - 1) ][changedX] != 'undefined' )
+         ) {
+            isNullOnTheBoard = true
+            for (var i = 0;i<letters.length && isNullOnTheBoard ;i++) {
+                nullCoordinates.push([changedY - (offset + i),changedX])
+                if (letters[i] != board[changedY - (offset + i)][changedX]) {
+                    nullCoordinates.length = 0
+                    isNullOnTheBoard = false
+                }
+            }
+         }
     }
     return isNullOnTheBoard
 }
 
 
 function keyPress( key ) {
+    if (isExploding) return 
+
     switch ( key ) {
         case 'left':
             if ( valid( -1 ) ) {
@@ -114,7 +201,7 @@ function keyPress( key ) {
             while( valid(0, 1) ) {
                 ++currentY;
             }
-            tick();
+            tick(false);
             break;
     }
 }
@@ -157,7 +244,7 @@ function newGame() {
     init();
     newShape();
     lose = false;
-    interval = setInterval( tick, 400 );
+    interval = setTimeout(tick, tickDuration );
 }
 
 function clearAllIntervals(){
